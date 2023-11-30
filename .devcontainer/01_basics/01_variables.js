28644f7a78fbe96dbd4ed3f421137bc2205a75b4
navneetbhadu <152082798+navneetbhadu@users.noreply.github.com>
2023-11-30 06:00:33 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "navneet@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2  //not allowed
+
+
+accountEmail = "nb@nb.com"
+accountPassword = "21212121"
+accountCity = "Bangluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
